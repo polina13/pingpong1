ac22076fd30f2c4b25e7004b2bd36c412b1bb46c
Polina <polina.mihova@yahoo.com>
2016-01-22 05:08:21 -0800

    Create specs showing number divisible by 3 and 5

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,33 @@
 var pingpong = function(number) {
-  var numbers = [];
-  for (var i=1; i <= number; i++) {
-    if (i % 15 === 0) {
-      numbers.push("pingpong");
-      return pingpong;
-    }else if ( i % 3 === 0) {
-      numbers.push("ping");
-    }else if ( i % 5 === 0) {
-      numbers.push("pong");
-    }else {
-      return false;
-      numbers.push(i);
-    }
-  };
 
-  $(document).ready(function () {
-    $("form#submit").submit(function(event) {
-      var number = parseInt($("input#inputnumber").val());
-      $(".output").append(numbers);
-    });
-    (event).preventDefault();
-  });
-    // return numbers;
+
+  for (var i=1; i <= number; i++) {
+    if ( (i % 3 === 0) && (i % 5 === 0)) {
+      number = "pingpong"
+    }
+  }
+
+    return number
+    // if (i % 15 === 0) {
+    //   numbers.push("pingpong");
+    //   return pingpong;
+    // }else if ( i % 3 === 0) {
+    //   numbers.push("ping");
+    // }else if ( i % 5 === 0) {
+    //   numbers.push("pong");
+    // }else {
+    //   return false;
+    //   numbers.push(i);
+    // }
+
+
+  // $(document).ready(function () {
+  //   $("form#submit").submit(function(event) {
+  //     var number = parseInt($("input#inputnumber").val());
+  //     $(".output").append(numbers);
+  //   });
+  //   (event).preventDefault();
+  //
+  // });
+  //   // return numbers;
 };
--- spec/specs.js
+++ spec/specs.js
@@ -1,13 +1,13 @@
 describe ('pingpong', function () {
-  it("is ping if a number is divisible by 3", function() {
-    expect(pingpong(9)).to.equal("ping");
+  it("is ping if a number is divisible by 5 and 3", function() {
+    expect(pingpong(15)).to.equal("pingpong");
   });
 
-  it("is pong if a number is divisible by 5", function() {
-    expect(pingpong(10)).to.equal("pong");
-  });
-
-  it("is pingpong if a number is divisible by 15", function() {
-    expect(pingpong(45)).to.equal("pingpong");
-  });
+  // it("is pong if a number is divisible by 5", function() {
+  //   expect(pingpong(10)).to.equal("pong");
+  // });
+  //
+  // it("is pingpong if a number is divisible by 15", function() {
+  //   expect(pingpong(45)).to.equal("pingpong");
+  // });
 });
