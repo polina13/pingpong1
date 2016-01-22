095f423aca32922e8a9c681a1608c2dd01d3e133
Polina <polina.mihova@yahoo.com>
2016-01-22 11:06:42 -0800

    Update specs file

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,49 @@
-var pingpong = function(number) {
-  if ((number % 3 === 0) && (number % 5 === 0)) {
-      return "pingpong";
+var numbersinarray = function(arrays) {
+  var listNum = [];
+  for (var i=1; i <= arrays; i++) {
+    listNum.push(i);
+  }
+  return listNum;
+}
+
+var pingpong = function(numbers) {
+  var listNum = numbersinarray(arrays);
+    for(var i=0; i < listNum.lenght ; i ++) {
+      if (listNum % 15 === 0) {
+        
+      }
     }
 }
-//   // }else if (number % 3 === 0) {
-//   //   return "ping";
-  // }
-  //
-  // } else if (number % 5 === 0) {
-  //   return "pong";
-  // }
-// };
+
+// var pingpong = function(upperLimit) {
+//   var numbersList = createNumberList(upperLimit);
+//   for (var i=0; i < numbersList.length; i++) {
+//     if (numbersList[i] % 15 === 0) {
+//       numbersList.splice(i, 1, "pingpong");
+//     } else if (numbersList[i] % 5 === 0) {
+//       numbersList.splice(i, 1, "pong");
+//     } else if (numbersList[i] % 3 === 0) {
+//       numbersList.splice(i, 1, "ping");
+//     }
+//   }
+//   console.log(numbersList);
+//   return numbersList;
+// }
+
+
+
+// var pingpong = function(number) {
+//   var numbes= [];
+//
+//   if ((number % 3 === 0) && (number % 5 === 0)) {
+//     return "pingpong";
+//     }
+//
+//   if (number % 3 === 0) {
+//     return "ping";
+//     }
+//
+//   if (number % 5 === 0) {
+//      return "pong"
+//     }
+//   }
--- spec/specs.js
+++ spec/specs.js
@@ -1,14 +1,20 @@
- describe ('pingpong', function () {
-  it("will be ping if a number is disible by 3 and it is replaced with ping in range of numbers", function () {
-    expect(pingpong(6)).to.equal("ping");
-  });
-  // it("will be pong if a number is divisible by 5", function() {
-  //   expect(pingpong(10)).to.equal("pong");
-  // });
-  it("is pingpong if a number is divisible by 15", function() {
-    expect(pingpong(45)).to.equal("pingpong");
+describe("numbersinarray", function () {
+  it("will transform integers into array up to 15", function() {
+    expect(numbersinarray(15)).to.eql([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]);
   });
 });
-//
-// it("will be ping if a number is disible by 3 and it is replaced with ping in range of numbers", function () {
-//   expect(pingpong(6)).to.eql([1,2,ping,3,4,5,ping]);
+
+describe("pingpong", function () {
+  it("will replace numbers divisible by 15 from the array with the word pingpong", function() {
+    expect(pingpong(15)).to.eql([1,2,"ping",4,"pong","ping",7,8,"ping","pong",11,"ping",13,14,"pingpong"]);
+  });
+
+  it("will replace numbers divisible by 5 from the loop with the word pong", function() {
+    expect(pingpong(10)).to.eql([1,2,"ping",4,"pong","ping",7,8,"ping","pong"]);
+  });
+
+  it("will replace numbers divisible by 3 from the array of with ping", function() {
+    expect(pingpong(9)).to.eql([1,2,"ping",4,"pong","ping",7,8,"ping"]);
+  });
+
+});
