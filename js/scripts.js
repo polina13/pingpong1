7e23b94345f2f7916a7c5b0e6b25568f82899f57
Polina <polina.mihova@yahoo.com>
2016-01-22 14:27:51 -0800

    Create four passing tests

--- js/scripts.js
+++ js/scripts.js
@@ -1,55 +1,56 @@
-var numbersinarray= function(number) {
-	var output = [];
-	for (var number = number; number > 0 ; number -= 1) {
-		integerArray.push(number);
-	}
-	return output;
+
+var numbersinarray = function(numberInput) {
+  var output = [];
+  for (var i=1; i <= numberInput; i++) {
+    output.push(i);
+  }
+  return output;
 };
 
-var divisiblebythree = function(number) {
-	if (number % 3 === 0) {
+
+var divisiblebythree = function(numberInput) {
+	if (numberInput % 3 === 0) {
 		return "ping";
 	}
 };
 
-var divisiblebyfive = function(number) {
-	if (number % 5 === 0) {
+var divisiblebyfive = function(numberInput) {
+	if (numberInput % 5 === 0) {
 		return "pong";
 	}
 };
 
-var divisiblebyfifteen = function(number) {
-	if ((number % 5 === 0) && (number % 3 === 0)) {
+var divisiblebyfifteen = function(numberInput) {
+	if ((numberInput % 5 === 0) && (numberInput % 3 === 0)) {
 		return "pingpong";
 	}
 };
 
-var pingpong = function(number) {
-	var output = createArray(number);
-
-	output.forEach(function(number, index, output) {
+ var pingpongfinal = function(numberInput) {
+    var output = numbersinarray(numberInput);
+	  output.forEach(function(number, index, output) {
 		if (divisiblebyfifteen(number) === 'pingpong') {
-			output[index] = 'pingpong';
+			output(index, "pingpong");
 		} else if (divisiblebythree(number) === 'ping') {
-			output[index] = 'ping';
+			output.splice(index, "ping");
 		} else if (divisiblebyfive(number) === 'pong') {
-			output[index] = 'pong';
+			output.splice(index, "pong");
 		}
 	});
 	return output;
-}
+};
 
 $(document).ready(function() {
 	$("button#submit").click(function(event){
 		event.preventDefault();
 
-		var number = $("input#writenumber").val();
-		var result = pingpong(number);
+		var numberInput = $("input#writenumber").val();
+		var finalResult = pingpongfinal(numberInput);
 
-		result.forEach(function(number) {
-			$("ul#output").prepend(" " + number + " ");
+		finalResult.forEach(function(number) {
+			$("ul#output").prepend("<li>" + number + "<li>");
 		});
-
-		$("input#writenumber").reset();
+    //
+		// $("input#numberInput").text(output);
 	});
 });
--- spec/specs.js
+++ spec/specs.js
@@ -1,23 +1,23 @@
-describe("numbersinarray", function () {
+describe('numbersinarray', function () {
   it("will transform integers into array up to 15", function() {
     expect(numbersinarray(15)).to.eql([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]);
   });
 });
 
-describe("divisiblebythree", function () {
+describe('divisiblebythree', function () {
   it("will take number that is divisible by 3 and will display 'ping'", function () {
     expect(divisiblebythree(9)).to.equal("ping");
   });
 });
 
 
-describe("divisiblebyfive", function () {
+describe('divisiblebyfive', function () {
   it("will take a number that is divisible by 5 and will display 'pong'", function () {
     expect(divisiblebyfive(10)).to.equal("pong");
   });
 });
 
-describe("divisiblebyfifteen", function () {
+describe('divisiblebyfifteen', function () {
   it("will take a number that is divisible by 5 and 3 and will display 'pingpong'", function () {
     expect(divisiblebyfifteen(15)).to.equal("pingpong");
   });
